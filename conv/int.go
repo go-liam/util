d5ad9eec242728b7086ab5109d658256dfd73080
liamA <37907909@qq.com>
2020-05-15 14:40:34 +0800

    feat: int bool ;

--- conv/int.go
+++ conv/int.go
@@ -1,3 +1,17 @@
 package conv
 
+// IntToBool :
+func IntToBool(v int) bool {
+	if v == 0 {
+		return false
+	}
+	return true
+}
 
+// BoolToInt :
+func BoolToInt(v bool) int {
+	if v {
+		return 1
+	}
+	return 0
+}
