475c282a6ed212327f2c2569b5e140d6e0dd5385
liamA <37907909@qq.com>
2020-05-26 15:05:06 +0800

    feat: time ;

--- conv/time.go
+++ conv/time.go
@@ -6,3 +6,11 @@ import "time"
 func TimeNowInt64() int64 {
 	return time.Now().Unix()
 }
+
+// TimeNowFormat :
+func TimeNowFormat(format string) string {
+	timeLocal, _ := time.LoadLocation("Asia/Chongqing")
+	time.Local = timeLocal
+	curNow := time.Now().Local()
+	return curNow.Format(format)
+}
--- conv/time_test.go
+++ conv/time_test.go
@@ -5,3 +5,8 @@ import "testing"
 func TestTimeNowInt64(t *testing.T) {
 	println(TimeNowInt64())
 }
+
+func TestTimeTimeNowFormat(t *testing.T) {
+	v1 := TimeNowFormat("2006-01-02 15:04:05")
+	println(v1)
+}
